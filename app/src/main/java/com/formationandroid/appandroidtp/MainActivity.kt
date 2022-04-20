3268281cd47eab07f41725eccb7ebea6b62ed172
nora-sch <nora.schumann.development@gmail.com>
2022-04-20 15:08:10 +0200

    Cas pratique 1 - installation du projet, affichage du log au lancement d'une activité

---
+++ app/src/main/java/com/formationandroid/appandroidtp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.formationandroid.appandroidtp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("montag", "Activité main se lance!")
+    }
+}
