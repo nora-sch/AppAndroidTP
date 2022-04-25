7d3669b3e31507f1f4023879a203872eae96c71d
nora-sch <nora.schumann.development@gmail.com>
2022-04-25 21:38:33 +0200

    Cas pratique 4 - envoi de la valeur saisie de l'activite 2 a l'activite 1 et afficher le resultat

--- app/src/main/java/com/formationandroid/appandroidtp/InputMainActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/InputMainActivity.kt
@@ -1,12 +1,16 @@
 package com.formationandroid.appandroidtp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,26 +18,54 @@ import kotlinx.coroutines.withContext
 
 class InputMainActivity : AppCompatActivity() {
 
+    companion object{
+       // const val TAG = "res";
+    }
+
     private lateinit var spinner : ProgressBar;
+    private lateinit var btn : AppCompatButton;
+    var actionDone : Boolean = false;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_main)
 
         spinner = findViewById(R.id.spinner);
+        btn = findViewById(R.id.button);
+        val retour = intent.getStringExtra("value");
+
+        if(retour != null){
+            actionDone = true;
+            spinner.setVisibility(View.VISIBLE)
+            btn.text = "ENVOYER";
+            val displayText : TextView = findViewById(R.id.text_received);
+            displayText.text = retour;
+        }
     }
 
     fun lancerPageSaisie(view: View) {
+        if(!actionDone){
         doStartSpinner();
+        }else{
+            shareData();
+        }
+    }
+
+    private fun shareData() {
+        //Log.i(TAG, "SHARE is HERE")
+
     }
 
     fun doStartSpinner(){
         spinner.setIndeterminate(true);
+
         CoroutineScope(Dispatchers.Default).launch{
             withContext(Dispatchers.Main) {spinner.setVisibility(View.VISIBLE)};
-            SystemClock.sleep(5000); // Sleep 5 seconds.
+            SystemClock.sleep(2000); // Sleep 2 seconds.
             val intent = Intent(this@InputMainActivity, InputSubmitActivity::class.java);
             startActivity(intent);
         }
+
     }
 }
--- app/src/main/java/com/formationandroid/appandroidtp/InputSubmitActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/InputSubmitActivity.kt
@@ -1,11 +1,22 @@
 package com.formationandroid.appandroidtp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class InputSubmitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_submit)
+    }
+
+    fun submitSaisie(view: View) {
+        val intent = Intent(this, InputMainActivity::class.java);
+        val textField : EditText = findViewById(R.id.saisie);
+        val incomingValue = textField.text.toString();
+        intent.putExtra("value", incomingValue);
+        startActivity(intent);
     }
 }
