0d27e17f2149b1eb6e7edbc04145eef7fd60240b
nora-sch <nora.schumann.development@gmail.com>
2022-04-25 16:04:21 +0200

    Cas pratique 3 - initialiser des valeurs de trois dés

--- app/src/main/java/com/formationandroid/appandroidtp/LanceurDesActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/LanceurDesActivity.kt
@@ -2,10 +2,30 @@ package com.formationandroid.appandroidtp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class LanceurDesActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "randomNb";
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lanceur_des)
+        val dice1 : TextView = findViewById(R.id.first_dice);
+        val dice2 : TextView = findViewById(R.id.second_dice);
+        val dice3 : TextView = findViewById(R.id.third_dice);
+
+        dice1.text = randomNb().toString();
+        dice2.text = randomNb().toString();
+        dice3.text = randomNb().toString();
+
+    }
+    fun randomNb() : Int{
+        var nb = Random.nextInt(1,6);
+        return nb;
     }
 }
