9d967eb9489e5974d6f6d58276b22271e9bbe38c
nora-sch <nora.schumann.development@gmail.com>
2022-04-25 15:10:07 +0200

    ajout scroll to top onclick button

--- app/src/main/java/com/formationandroid/appandroidtp/ArticleActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/ArticleActivity.kt
@@ -1,18 +1,37 @@
 package com.formationandroid.appandroidtp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.ScrollView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 
 
 class ArticleActivity : AppCompatActivity() {
+    private lateinit var scrollView: ScrollView;
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
 
         Toast.makeText(getApplicationContext(),
             "Bienvenue!",
             Toast.LENGTH_LONG).show();
+        scrollView = findViewById(R.id.scroll_view_article);
+
+
+        val bouton: AppCompatButton = findViewById(R.id.btn_retour);
+
+        //val bouton: Button = findViewById(R.id.btn_retour);
+         bouton.setOnClickListener{
+           Toast.makeText(getApplicationContext(),
+                "Vous êtes revenu \n" + "au début de l'article!",
+                Toast.LENGTH_LONG).show();
+             scrollView.scrollTo(0,0);
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/formationandroid/appandroidtp/LanceurDesActivity.kt
@@ -0,0 +1,11 @@
+package com.formationandroid.appandroidtp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LanceurDesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lanceur_des)
+    }
+}
