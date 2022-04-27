d673af95a4fa75af0547c5c5e0b143a28f0dd08b
nora-sch <nora.schumann.development@gmail.com>
2022-04-27 22:39:14 +0200

    Cas pratique 10 - action bar, icone menu, affichage de legende coins arrondis, diferents fonts

--- app/src/main/java/com/formationandroid/appandroidtp/activities/MemoActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/activities/MemoActivity.kt
@@ -1,11 +1,18 @@
 package com.formationandroid.appandroidtp.activities
 
+import android.content.ClipData
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -19,11 +26,12 @@ class MemoActivity : AppCompatActivity() {
 
     private lateinit var memoAdapter : MemoAdapter;
     private lateinit var recyclerView: RecyclerView;
+    private var favoriSelected = false;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo)
-
+        favoriSelected = false;
         val preferences = PreferenceManager.getDefaultSharedPreferences(this);
         if(preferences.getInt("idMemo", 0) != 0) {
              Toast.makeText(this, preferences.getInt("idMemo", 0).toString(), Toast.LENGTH_LONG).show();
@@ -60,5 +68,36 @@ class MemoActivity : AppCompatActivity() {
                 recyclerView.smoothScrollToPosition(0);
                 textSaisieField.setText("");
             }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
+        menuInflater.inflate(R.menu.memo_menu, menu);
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        return when (item.itemId)
+        {
+
+            R.id.action_favori ->
+            {
+                val legende : TextView = findViewById(R.id.legende_favori);
+                if(favoriSelected) {
+                    Log.i("tag", "not-favori");
+                    favoriSelected = false;
+                    item.setIcon(R.drawable.ic_favorite_false);
+                    legende.isVisible = false;
+                    false
+                }else{
+                    Log.i("tag", "favori");
+                    favoriSelected = true;
+                    item.setIcon(R.drawable.ic_favorite_true);
+                    legende.isVisible = true;
+                    true
+                }
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
