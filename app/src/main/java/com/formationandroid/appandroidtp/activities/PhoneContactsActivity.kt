714c29c3b7b7f086341119a460f9f536af3efe69
nora-sch <nora.schumann.development@gmail.com>
2022-04-28 12:18:27 +0200

    Cas pratique 13 - recevoir le choix de permission d'utilisateur

--- app/src/main/java/com/formationandroid/appandroidtp/activities/PhoneContactsActivity.kt
+++ app/src/main/java/com/formationandroid/appandroidtp/activities/PhoneContactsActivity.kt
@@ -11,6 +11,9 @@ import com.formationandroid.appandroidtp.R
 
 
 class PhoneContactsActivity : AppCompatActivity() {
+    companion object{
+        private const val REQUEST_CODE_CONTACTS = 111;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_contacts)
@@ -19,10 +22,25 @@ class PhoneContactsActivity : AppCompatActivity() {
         if(permission == PackageManager.PERMISSION_GRANTED){
             lireContacts();
         }else{
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 111);
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), REQUEST_CODE_CONTACTS);
         }
     }
     private fun lireContacts(){
         Toast.makeText(this, "Accès aux contacts", Toast.LENGTH_LONG).show();
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_CODE_CONTACTS){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                lireContacts();
+            }else{
+                Toast.makeText(this, "Autorisation refusée", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
